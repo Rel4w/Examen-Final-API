032893578791f6e1ef443451799f36768b2330b3
Jonathan <hei.jonathan.4@gmail.com>
2025-09-03 10:03:01 +0300

    remove commentary

--- main.py
+++ main.py
@@ -16,23 +16,19 @@ class Phone(BaseModel):
 
 phones_db: List[Phone] = []
 
-#GET /health
 @app.get("/health")
 def health_check():
     return "Ok"
 
-#POST /phones
 @app.post("/phones", status_code=201)
 def create_phone(phone: Phone):
     phones_db.append(phone)
     return {"message": "Phone created successfully", "phone": phone}
 
-#GET /phones
 @app.get("/phones", response_model=List[Phone])
 def get_all_phones():
     return phones_db
 
-#GET /phones/{id}
 @app.get("/phones/{id}", response_model=Phone)
 def get_phone(id: str):
     for phone in phones_db:
@@ -40,7 +36,6 @@ def get_phone(id: str):
             return phone
     raise HTTPException(status_code=404, detail="Phone not found")
 
-#PUT /phones/{id}/characteristics
 @app.put("/phones/{id}/characteristics", response_model=Phone)
 def update_characteristics(id: str, characteristics: Characteristic):
     for phone in phones_db:
